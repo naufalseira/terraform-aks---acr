28d2e381da674a85996847a0dac87b114226af65
naufalseira <kingnaufal3@gmail.com>
2024-12-29 17:57:24 +0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,19 @@ output "kubernetes_cluster_dns_prefix" {
   value = azurerm_kubernetes_cluster.aks.dns_prefix
 }
 
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_admin_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
+
 output "kubernetes_cluster_kube_config_raw" {
   value = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
